ad523ea7f1f592f3a805de8c13d596d456d530d3
martas <m.sroczynska@student.uw.edu.pl>
2016-05-19 15:35:41 +0200

    finished work for today

---
+++ files_operations.pyx
@@ -0,0 +1,22 @@
+
+
+
+# list_of_f_values = []
+# list_of_r_values = []
+#
+# for j in range(n_states):
+
+# def get_function():
+#     r_values = []
+#     f_values = []
+#     with open("/home/martas/numerov_11_5_normalized_dr_const1D/psi_l=" + str(l[j]) + "_" + str(energy[j]) + '_normalized_dr_const1D.dat') as data:
+#         counter = 0
+#         for lin in csv.reader(data, dialect="excel-tab"):
+#             if float(lin[0]) != 0 and float(lin[0]) < r_max_value[j]:
+#                 r_values.append(float(lin[0]))
+#                 f_values.append(float(lin[1]) / math.fabs(float(lin[0])))
+#             counter += 1
+#         r_values.append(r_max_value[j])
+#         f_values.append(0)
+# list_of_r_values.append(r_values)
+# list_of_f_values.append(f_values)
